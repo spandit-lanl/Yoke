b8b6794cdee53f7bd60a0682acfc3c2bad6cc281
Kyle Scott Hickmann <hickmank@lanl.gov>
2024-06-05 12:48:23 -0600

    Added environment variables for use on dodona in commented lines

--- yoke_env_setup.sh
+++ yoke_env_setup.sh
@@ -2,6 +2,8 @@
 
 # Bash script to setup some YOKE-specific enviroment variables.
 
+# ON burr.lanl.gov
+#
 # Base directory where YOKE is housed
 export YOKE_DIR='/data2/yoke/'
 
@@ -22,5 +24,29 @@ export LSC_DESIGN_DIR='/data2/'
 
 # LSC NPZ data directory
 export LSC_NPZ_DIR='/data2/lsc240420/'
+
+
+# # ON dodona.lanl.gov
+# #
+# # Base directory where YOKE is housed
+# export YOKE_DIR='/data/<username>/yoke/'
+
+# # Base anaconda install directory
+# export YOKE_CONDA='/data/anaconda3b/'
+
+# # Torch anaconda environment to use
+# export YOKE_TORCH='torch_gpu_240320'
+
+# # NC design-file directory
+# export NC_DESIGN_DIR='/data1/'
+
+# # NC NPZ data directory
+# export NC_NPZ_DIR='/data1/nc231213_npzs/'
+
+# # LSC design-file directory
+# export LSC_DESIGN_DIR='/data1/'
+
+# # LSC NPZ data directory
+# export LSC_NPZ_DIR='/data1/lsc240420/'
 
 
