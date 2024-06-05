ce65a27792735bdfa594e26bc815297a7e991e86
Kyle Hickmann <hickmank@lanl.gov>
2024-06-05 17:24:55 -0600

    Added wx implementation of parameters2image
    Former-commit-id: 4c69c47309bb84143821fcf6d39aa1a55afc414c

--- evaluation/parameters2image_slider.py
+++ evaluation/parameters2image_slider.py
@@ -14,7 +14,7 @@ from models.surrogateCNNmodules import jekelCNNsurrogate
 import torch_training_utils as tr
 
 import wx
-import PIL import Image
+from PIL import Image
 
 # Imports for plotting
 # To view possible matplotlib backends use
@@ -109,60 +109,383 @@ checkpoint_epoch = tr.load_model_and_optimizer_hdf5(model,
 ## Input parameters and evaluation
 ###################################
 
-# Load single image and parameters pair
-# sa1, sa2, sa3, sa4, sa5, sa6, sa7,
-# st1, st2, st3, st4, st5, st6, st7,
-# tt1, tt2, tt3, tt4, tt5, tt6, tt7,
-# ct1, ct2, ct3, ct4, ct5, ct6, ct7, time
-input_params = np.array([[4.5, 5.0, 5.5, 7.0, 8.5, 10.0, 10.5,
-                         0.5, 0.25, 0.25, 0.25, 0.25, 0.3, 0.3,
-                         0.4, 0.35, 0.25, 0.25, 0.2, 0.1, 0.1,
-                          0.1, 0.1, 0.1, 0.1, 0.15, 0.15, 0.2, 25.0]],
-                        dtype=np.float32)
-input_params = torch.from_numpy(input_params)
 
-# Evaluate model
-model.eval()
-pred_image = model(input_params)
+# # Plot normalized radiograph and density field for diagnostics.
+# fig1, ax1 = plt.subplots(1, 1, figsize=(12, 12))
+# fig1.suptitle('Time={:.3f}us'.format(input_params[-1]), fontsize=18)
+# img1 = ax1.imshow(pred_image,
+#                   aspect='equal',
+#                   origin='lower',
+#                   cmap='jet',
+#                   vmin=pred_image.min(),
+#                   vmax=pred_image.max())
+# ax1.set_ylabel("Z-axis", fontsize=16)                 
+# ax1.set_xlabel("R-axis", fontsize=16)
+# ax1.set_title('Prediction', fontsize=18)
 
-#print('Shape of inputs:', input_params.shape)
-#print('Prediction shape:', pred_image.shape)
+# divider1 = make_axes_locatable(ax1)
+# cax1 = divider1.append_axes('right', size='10%', pad=0.1)
+# fig1.colorbar(img1,
+#               cax=cax1).set_label('Density (g/cc)',
+#                                   fontsize=14)
 
-# Reshape for plotting
-input_params = np.squeeze(input_params.numpy())
-# Predictions from network must be detached from gradients in order to be
-# written to numpy arrays.
-pred_image = np.squeeze(pred_image.detach().numpy())
-#print('Shape of image prediction:', pred_image.shape)
+# # Save or plot images
+# if SAVEFIG:
+#     if not os.path.exists(savedir):
+#         os.makedirs(savedir)
 
-# Plot normalized radiograph and density field for diagnostics.
-fig1, ax1 = plt.subplots(1, 1, figsize=(12, 12))
-fig1.suptitle('Time={:.3f}us'.format(input_params[-1]), fontsize=18)
-img1 = ax1.imshow(pred_image,
-                  aspect='equal',
-                  origin='lower',
-                  cmap='jet',
-                  vmin=pred_image.min(),
-                  vmax=pred_image.max())
-ax1.set_ylabel("Z-axis", fontsize=16)                 
-ax1.set_xlabel("R-axis", fontsize=16)
-ax1.set_title('Prediction', fontsize=18)
-
-divider1 = make_axes_locatable(ax1)
-cax1 = divider1.append_axes('right', size='10%', pad=0.1)
-fig1.colorbar(img1,
-              cax=cax1).set_label('Density (g/cc)',
-                                  fontsize=14)
-
-# Save or plot images
-if SAVEFIG:
-    if not os.path.exists(savedir):
-        os.makedirs(savedir)
-
-    plt.figure(fig1.number)
-    filenameA = f'{savedir}/jCNN_pred_image.png'
-    plt.savefig(filenameA, bbox_inches='tight')
-else:
-    plt.show()
+#     plt.figure(fig1.number)
+#     filenameA = f'{savedir}/jCNN_pred_image.png'
+#     plt.savefig(filenameA, bbox_inches='tight')
+# else:
+#     plt.show()
 
 
+def run_jCNN(sa1, sa2, sa3, sa4, sa5, sa6, sa7,
+             st1, st2, st3, st4, st5, st6, st7,
+             tt1, tt2, tt3, tt4, tt5, tt6, tt7,
+             ct1, ct2, ct3, ct4, ct5, ct6, ct7, time):
+    """Function to evaluate loaded model on a set of inputs.
+
+    """
+    # input_params = np.array([[4.5, 5.0, 5.5, 7.0, 8.5, 10.0, 10.5,
+    #                           0.5, 0.25, 0.25, 0.25, 0.25, 0.3, 0.3,
+    #                           0.4, 0.35, 0.25, 0.25, 0.2, 0.1, 0.1,
+    #                           0.1, 0.1, 0.1, 0.1, 0.15, 0.15, 0.2, 25.0]],
+    #                         dtype=np.float32)
+    input_params = np.array([[sa1, sa2, sa3, sa4, sa5, sa6, sa7,
+                              st1, st2, st3, st4, st5, st6, st7,
+                              tt1, tt2, tt3, tt4, tt5, tt6, tt7,
+                              ct1, ct2, ct3, ct4, ct5, ct6, ct7, time]],
+                            dtype=np.float32)
+    input_params = torch.from_numpy(input_params)
+
+    # Evaluate model
+    model.eval()
+    pred_image = model(input_params)
+
+    #print('Shape of inputs:', input_params.shape)
+    #print('Prediction shape:', pred_image.shape)
+
+    # Reshape for plotting
+    input_params = np.squeeze(input_params.numpy())
+    # Predictions from network must be detached from gradients in order to be
+    # written to numpy arrays.
+    pred_image = np.squeeze(pred_image.detach().numpy())
+    #print('Shape of image prediction:', pred_image.shape)
+
+    return pred_image.astype(np.uint8)
+
+
+class MyFrame(wx.Frame):
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, title='Jekel-tCNN GUI', size=(350, 400))
+        
+        self.panel = wx.Panel(self)
+        self.init_ui()
+
+    def init_ui(self):
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        
+        # Create sliders
+        #sa1, sa2, sa3, sa4, sa5, sa6, sa7,
+        #st1, st2, st3, st4, st5, st6, st7,
+        #tt1, tt2, tt3, tt4, tt5, tt6, tt7,
+        #ct1, ct2, ct3, ct4, ct5, ct6, ct7, time
+        self.slider_sa1 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_sa1.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_sa1, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_sa2 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_sa2.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_sa2, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_sa3 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_sa3.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_sa3, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_sa4 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_sa4.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_sa4, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_sa5 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_sa5.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_sa5, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_sa6 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_sa6.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_sa6, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_sa7 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_sa7.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_sa7, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_st1 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_st1.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_st1, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_st2 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_st2.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_st2, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_st3 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_st3.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_st3, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_st4 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_st4.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_st4, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_st5 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_st5.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_st5, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_st6 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_st6.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_st6, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_st7 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_st7.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_st7, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_tt1 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_tt1.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_tt1, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_tt2 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_tt2.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_tt2, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_tt3 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_tt3.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_tt3, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_tt4 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_tt4.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_tt4, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_tt5 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_tt5.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_tt5, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_tt6 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_tt6.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_tt6, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_tt7 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_tt7.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_tt7, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_ct1 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_ct1.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_ct1, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_ct2 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_ct2.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_ct2, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_ct3 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_ct3.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_ct3, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_ct4 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_ct4.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_ct4, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_ct5 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_ct5.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_ct5, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_ct6 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_ct6.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_ct6, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_ct7 = wx.Slider(self.panel,
+                                    value=125,
+                                    minValue=0,
+                                    maxValue=255,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_ct7.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_ct7, flag=wx.EXPAND|wx.ALL, border=10)
+
+        self.slider_time = wx.Slider(self.panel,
+                                     value=125,
+                                     minValue=0,
+                                     maxValue=255,
+                                     style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.slider_time.Bind(wx.EVT_SLIDER, self.on_slide)
+        vbox.Add(self.slider_time, flag=wx.EXPAND|wx.ALL, border=10)
+
+        # Set up an area for displaying the image
+        self.image_panel = wx.StaticBitmap(self.panel)
+        vbox.Add(self.image_panel, proportion=1, flag=wx.EXPAND|wx.ALL, border=10)
+        
+        self.panel.SetSizer(vbox)
+        self.update_image()
+
+    def on_slide(self, event):
+        self.update_image()
+        
+    def update_image(self):
+        # Get the current slider values
+        sa1 = self.slider_sa1.GetValue()
+        sa2 = self.slider_sa2.GetValue()
+        sa3 = self.slider_sa3.GetValue()
+        sa4 = self.slider_sa4.GetValue()
+        sa5 = self.slider_sa5.GetValue()
+        sa6 = self.slider_sa6.GetValue()
+        sa7 = self.slider_sa7.GetValue()
+        
+        st1 = self.slider_st1.GetValue()
+        st2 = self.slider_st2.GetValue()
+        st3 = self.slider_st3.GetValue()
+        st4 = self.slider_st4.GetValue()
+        st5 = self.slider_st5.GetValue()
+        st6 = self.slider_st6.GetValue()
+        st7 = self.slider_st7.GetValue()
+        
+        tt1 = self.slider_tt1.GetValue()
+        tt2 = self.slider_tt2.GetValue()
+        tt3 = self.slider_tt3.GetValue()
+        tt4 = self.slider_tt4.GetValue()
+        tt5 = self.slider_tt5.GetValue()
+        tt6 = self.slider_tt6.GetValue()
+        tt7 = self.slider_tt7.GetValue()
+        
+        ct1 = self.slider_ct1.GetValue()
+        ct2 = self.slider_ct2.GetValue()
+        ct3 = self.slider_ct3.GetValue()
+        ct4 = self.slider_ct4.GetValue()
+        ct5 = self.slider_ct5.GetValue()
+        ct6 = self.slider_ct6.GetValue()
+        ct7 = self.slider_ct7.GetValue()
+        
+        time = self.slider_time.GetValue()
+        
+        # Run the neural network and get a 2D numpy array
+        array = run_jCNN(sa1, sa2, sa3, sa4, sa5, sa6, sa7,
+                         st1, st2, st3, st4, st5, st6, st7,
+                         tt1, tt2, tt3, tt4, tt5, tt6, tt7,
+                         ct1, ct2, ct3, ct4, ct5, ct6, ct7, time)
+        
+        # Convert numpy array to PIL Image and then to wx.Image
+        img = Image.fromarray(array, 'L')  # 'L' mode for grayscale
+        wx_img = wx.Image(img.size[0], img.size[1])
+        wx_img.SetData(img.convert("RGB").tobytes())
+        
+        # Update the displayed image
+        self.image_panel.SetBitmap(wx.Bitmap(wx_img))
+        self.Refresh()
+
+app = wx.App(False)
+frame = MyFrame(None)
+frame.Show(True)
+app.MainLoop()
--- yoke_env_setup.sh
+++ yoke_env_setup.sh
@@ -2,28 +2,40 @@
 
 # Bash script to setup some YOKE-specific enviroment variables.
 
-# ON burr.lanl.gov
+# ON OSX local
 #
 # Base directory where YOKE is housed
-export YOKE_DIR='/data2/yoke/'
+export YOKE_DIR='/Users/hickmank/yoke'
 
 # Base anaconda install directory
-export YOKE_CONDA='/data1/anaconda3/'
+export YOKE_CONDA='/Users/hickmank/anaconda3/'
 
 # Torch anaconda environment to use
-export YOKE_TORCH='torch_gpu_240403'
+export YOKE_TORCH='torch_osx_240605'
 
-# NC design-file directory
-export NC_DESIGN_DIR='/data2/'
 
-# NC NPZ data directory
-export NC_NPZ_DIR='/data2/nc231213_npzs/'
+# # ON burr.lanl.gov
+# #
+# # Base directory where YOKE is housed
+# export YOKE_DIR='/data2/yoke/'
 
-# LSC design-file directory
-export LSC_DESIGN_DIR='/data2/'
+# # Base anaconda install directory
+# export YOKE_CONDA='/data1/anaconda3/'
 
-# LSC NPZ data directory
-export LSC_NPZ_DIR='/data2/lsc240420/'
+# # Torch anaconda environment to use
+# export YOKE_TORCH='torch_gpu_240403'
+
+# # NC design-file directory
+# export NC_DESIGN_DIR='/data2/'
+
+# # NC NPZ data directory
+# export NC_NPZ_DIR='/data2/nc231213_npzs/'
+
+# # LSC design-file directory
+# export LSC_DESIGN_DIR='/data2/'
+
+# # LSC NPZ data directory
+# export LSC_NPZ_DIR='/data2/lsc240420/'
 
 
 # # ON dodona.lanl.gov
