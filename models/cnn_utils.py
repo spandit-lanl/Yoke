7077092629eaea4d2bcb51f783f1ea366e2d9c1a
Kyle Scott Hickmann <hickmank@lanl.gov>
2024-06-07 20:10:41 -0600

    Setup training and model for non-interpolative surrogate CNN
    Former-commit-id: 0cb32fb8feaad6b4473ddd2374c3f25119a1e74e

--- models/cnn_utils.py
+++ models/cnn_utils.py
@@ -39,7 +39,7 @@ def conv2d_shape(w, h, k, s_w, s_h, p_w, p_h):
     return new_w, new_h, total
 
 
-def convtranspose2d_shape(w, h, k_w, k_h, s_w, s_h, p_w, p_h):
+def convtranspose2d_shape(w, h, k_w, k_h, s_w, s_h, p_w, p_h, op_w, op_h, d_w, d_h):
     """Function to calculate the new dimension of an image after a
     nn.ConvTranspose2d. This assumes *groups*, *dilation*, and *ouput_padding*
     are all default values.
@@ -53,6 +53,10 @@ def convtranspose2d_shape(w, h, k_w, k_h, s_w, s_h, p_w, p_h):
         s_h (int): stride size along the height
         p_w (int): padding size along the width
         p_h (int): padding size along the height
+        op_w (int): output padding size along the width
+        op_h (int): output padding size along the height
+        d_w (int): dilation size along the width
+        d_h (int): dilation size along the height
 
     Returns:
         new_w (int): number of pixels along the width
@@ -65,8 +69,8 @@ def convtranspose2d_shape(w, h, k_w, k_h, s_w, s_h, p_w, p_h):
 
     """
 
-    new_w = (w - 1)*s_w - 2*p_w + (k_w - 1) + 1
-    new_h = (h - 1)*s_h - 2*p_h + (k_h - 1) + 1
+    new_w = (w - 1)*s_w - 2*p_w + d_w*(k_w - 1) + op_w + 1
+    new_h = (h - 1)*s_h - 2*p_h + d_h*(k_h - 1) + op_h + 1
     total = new_w * new_h
 
     return new_w, new_h, total
