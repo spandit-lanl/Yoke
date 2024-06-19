a56d4062247db6191482fcfa8bdd63ed65e68834
Kyle Scott Hickmann <hickmank@lanl.gov>
2024-06-19 13:20:06 -0600

    Added some parameters to hyperparameter.py created scheduler training
    Former-commit-id: a61d24c7e6256e5d136dc2c2425bf0521e3e62c4

--- yoke_env_setup.sh
+++ yoke_env_setup.sh
@@ -20,10 +20,10 @@
 export YOKE_DIR='/data2/yoke/'
 
 # Base anaconda install directory
-export YOKE_CONDA='/data1/anaconda3/'
+export YOKE_CONDA='/data/anaconda3/'
 
 # Torch anaconda environment to use
-export YOKE_TORCH='torch_gpu_240403'
+export YOKE_TORCH='torch_gpu_240619'
 
 # NC design-file directory
 export NC_DESIGN_DIR='/data2/'
