cff38ca5d0eacff5a339489498012a7b695e1f7a
Sharmistha Chakrabarti - 368208 <schakrabarti@darwin-fe3.lanl.gov>
2024-10-08 13:44:28 -0600

    Revert "Added pytests"
    This reverts commit e45c48145a5abd0998da2346b665ad119e5eaea3 [formerly 9173c8e590eea2c7c36120d0ff08035ae7a51ee1].
    
    
    Former-commit-id: 41abc7add1c36928df2a04fd9fdecf8f41f00580

--- unit_test_pytests/sorted_list.py
+++
@@ -1,4 +0,0 @@
-def sorted_list(list_numbers):
-    if not isinstance(list_numbers, list):
-        raise ValueError("Input should be a list")
-    return sorted(set(list_numbers))
--- unit_test_pytests/tensor_operations.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-def add_tensors(tensor_a, tensor_b):
-    """ Function that adds two PyTorch tensors. """
-    return torch.add(tensor_a, tensor_b)
