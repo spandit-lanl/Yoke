28499d7a775fc88ae2392423be51328af86c91e9
Andrew Koldewey Henrick <ahenrick@lanl.gov>
2024-10-21 11:27:28 -0600

    Correct missing returns on lambdas to defs in jwl_dataset.py; update samples_sand-all.csv to have correct CJ state and release energies.
    Former-commit-id: 54dee79a0a601294169eb584e7851b15a8f22797

--- src/yoke/datasets/jwl_dataset.py
+++ src/yoke/datasets/jwl_dataset.py
@@ -17,7 +17,7 @@ def compute_CJ(
     Args: A,B,C in units of pressure
         G, R1, R2 are unitless
         v0 is in units of specific volume
-    Returns: vj/v0 [unitless], pj [pressure], Dj = [velocity=sqrt(energy)],
+    Returns: Dj = [velocity=sqrt(energy)], pj [pressure], vj/v0 [unitless], 
              edet [specific energy=pressure x specific volume]
     """
     # ideal gas cp/cv constant descibing large expansion behavior
@@ -25,13 +25,13 @@ def compute_CJ(
     kp1 = k + 1
 
     # reference isentrope: ps(v/v0)
-    def ps(vs: float) -> float: A * np.exp(-R1 * vs) + B * np.exp(-R2 * vs) + C / vs**k
+    def ps(vs: float) -> float: return A * np.exp(-R1 * vs) + B * np.exp(-R2 * vs) + C / vs**k
     # adiabatic gamma on reference isentrope: gs(v/v0)
     def gs(vs: float) -> float:
-        (vs * (A * R1 * np.exp(-R1 * vs) + B * R2 * np.exp(-R2 * vs) + C * k / vs**kp1)
+        return (vs * (A * R1 * np.exp(-R1 * vs) + B * R2 * np.exp(-R2 * vs) + C * k / vs**kp1)
          / ps(vs))
     # at CJ: vj/v0 = gj/(gj+1) assuming p0=0
-    def fzero(vs: float) -> float: gs(vs) * (vs - 1) + vs
+    def fzero(vs: float) -> float: return gs(vs) * (vs - 1) + vs
     vsj = root(fzero, 0.5, tol=1e-14).x[0]
     pj = ps(vsj)
     Dj = np.sqrt(pj * v0 / (1 - vsj))
@@ -42,7 +42,7 @@ def compute_CJ(
         + B * (G / R2 / vsj - 1) * np.exp(-R2 * vsj)
     ) + pj / 2 * v0 * (vsj - 1)
 
-    return vsj, pj, Dj, edet
+    return Dj, pj, vsj, edet
 
 
 def compute_e_release( A: float, B: float, C: float, G: float,
@@ -57,7 +57,7 @@ def compute_e_release( A: float, B: float, C: float, G: float,
         vs=v/v0 is the number of initial volumes of expansion
     Returns: detonation energy released in units of specific energy
     """
-    def Int(vs: float) -> float: v0 * (
+    def Int(vs: float) -> float: return v0 * (
         A / R1 * np.exp(-R1 * vs) + B / R2 * np.exp(-R2 * vs) + C / G / vs**G
     )
     return edet - Int(vs)
@@ -98,7 +98,7 @@ class CYLEX_pdv2jwl_Dataset(Dataset):
         self.df = df = df.iloc[rng]
 
         self.Nsamples = len(df)
-        # self.resetCJ()
+        #self.resetCJ()
 
     def resetCJ(self) -> None:
         """Reset CJ and expansion energy for given JWL parameter."""
@@ -107,7 +107,7 @@ class CYLEX_pdv2jwl_Dataset(Dataset):
             chks = row["dcj":"edet"]
             es = row["e1":"e7"]
 
-            vsj, pj, Dj, edet = compute_CJ(*jwls.values)
+            Dj, pj, vsj, edet = compute_CJ(*jwls.values)
             chks["dcj"] = Dj
             chks["pcj"] = pj
             chks["vcj"] = vsj * row["V0"]
@@ -174,7 +174,7 @@ class CYLEXnorm_pdv2jwl_Dataset(Dataset):
             chks = row["dcj":"edet"]
             es = row["e1":"e7"]
 
-            vsj, pj, Dj, edet = compute_CJ(*jwls.values)
+            Dj, pj, vsj, edet = compute_CJ(*jwls.values)
             chks["dcj"] = Dj
             chks["pcj"] = pj
             chks["vcj"] = vsj * row["V0"]
@@ -207,83 +207,83 @@ class CYLEXnorm_pdv2jwl_Dataset(Dataset):
         return input, output
 
 
-# if __name__ == "__main__":
-#     """For testing and debugging.
+if __name__ == "__main__":
+    """For testing and debugging.
 
-#     """
+    """
 
-#     # Imports for plotting
-#     # To view possible matplotlib backends use
-#     # >>> import matplotlib
-#     # >>> bklist = matplotlib.rcsetup.interactive_bk
-#     # >>> print(bklist)
-#     import matplotlib.pyplot as plt
-#     import matplotlib
+    # Imports for plotting
+    # To view possible matplotlib backends use
+    # >>> import matplotlib
+    # >>> bklist = matplotlib.rcsetup.interactive_bk
+    # >>> print(bklist)
+    import matplotlib.pyplot as plt
+    import matplotlib
 
-#     matplotlib.use("MacOSX")
-#     #     matplotlib.use('TkAgg')
-#     #     # Get rid of type 3 fonts in figures
-#     #     matplotlib.rcParams['pdf.fonttype'] = 42
-#     #     matplotlib.rcParams['ps.fonttype'] = 42
-#     #     import matplotlib.pyplot as plt
-#     #     # Ensure LaTeX font
-#     #     font = {'family': 'serif'}
-#     #     plt.rc('font', **font)
-#     #     plt.rcParams['figure.figsize'] = (6, 6)
-#     #     from mpl_toolkits.axes_grid1 import make_axes_locatable
+    matplotlib.use("MacOSX")
+    #     matplotlib.use('TkAgg')
+    #     # Get rid of type 3 fonts in figures
+    #     matplotlib.rcParams['pdf.fonttype'] = 42
+    #     matplotlib.rcParams['ps.fonttype'] = 42
+    #     import matplotlib.pyplot as plt
+    #     # Ensure LaTeX font
+    #     font = {'family': 'serif'}
+    #     plt.rc('font', **font)
+    #     plt.rcParams['figure.figsize'] = (6, 6)
+    #     from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-#     # load data and get initial stats
-#     data_df = pd.read_csv("samples_sand-diff.csv", sep=",", header=0, engine="python")
-#     print(data_df.keys())
-#     print(data_df.describe().loc[:, "dcj":"edet"])
-#     print(data_df.describe().loc[:, "edet":"e7"])
+    # load data and get initial stats
+    data_df = pd.read_csv("samples_sand-all.csv", sep=",", header=0, engine="python")
+    print(data_df.keys())
+    print(data_df.describe().loc[:, "dcj":"edet"])
+    print(data_df.describe().loc[:, "edet":"e7"])
 
-#     # create Dataset object and access for plotting
-#     cylex = CYLEX_pdv2jwl_Dataset(slice(0, None), "samples_sand-diff.csv")
+    # create Dataset object and access for plotting
+    cylex = CYLEX_pdv2jwl_Dataset(slice(0, None), "samples_sand-all.csv")
 
-#     f, aax = plt.subplots(1, 2)
-#     tstr = data_df.keys().to_series()["t0.1":"t4.5"].values
-#     tt = np.array([float(s[1:]) for s in tstr])
-#     tt = tt - tt[0]
-#     jwlstr = data_df.keys().to_series()["a":"r2"].values
-#     for i in range(0, len(cylex)):
-#         aax[0].plot(tt, cylex[i][0], "x")
-#         aax[1].semilogy(cylex[i][1], "x")
-#     ax = aax[0]
+    f, aax = plt.subplots(1, 2)
+    tstr = data_df.keys().to_series()["t0.1":"t4.5"].values
+    tt = np.array([float(s[1:]) for s in tstr])
+    tt = tt - tt[0]
+    jwlstr = data_df.keys().to_series()["a":"r2"].values
+    for i in range(0, len(cylex)):
+        aax[0].plot(tt, cylex[i][0], "x")
+        aax[1].semilogy(cylex[i][1], "x")
+    ax = aax[0]
 
-#     # load Pemberton 2011 Cylinder test data for comparison
-#     for sn in range(1, 9):
-#         S_df = pd.read_csv(
-#             "Shot0" + str(sn) + "velocities.txt",
-#             sep=",",
-#             header=7,
-#             engine="python",
-#             skipinitialspace=True,
-#         )
-#         S_df = S_df.T.drop_duplicates().T
-#         i = 1
-#         cols = {}
-#         for k in S_df.keys():
-#             nn = k
-#             pp = k.split(" ")
-#             if pp[0] == "Time":
-#                 nn = pp[0] + " " + str(i) + " " + pp[1].split(".")[0]
-#             elif pp[0] == "Sigma_Y":
-#                 nn = pp[0] + " " + str(i) + " " + pp[1].split(".")[0]
-#                 i = i + 1
-#             cols[k] = nn
-#         S_df = S_df.rename(columns=cols)
+    # # load Pemberton 2011 Cylinder test data for comparison
+    # for sn in range(1, 9):
+    #     S_df = pd.read_csv(
+    #         "Shot0" + str(sn) + "velocities.txt",
+    #         sep=",",
+    #         header=7,
+    #         engine="python",
+    #         skipinitialspace=True,
+    #     )
+    #     S_df = S_df.T.drop_duplicates().T
+    #     i = 1
+    #     cols = {}
+    #     for k in S_df.keys():
+    #         nn = k
+    #         pp = k.split(" ")
+    #         if pp[0] == "Time":
+    #             nn = pp[0] + " " + str(i) + " " + pp[1].split(".")[0]
+    #         elif pp[0] == "Sigma_Y":
+    #             nn = pp[0] + " " + str(i) + " " + pp[1].split(".")[0]
+    #             i = i + 1
+    #         cols[k] = nn
+    #     S_df = S_df.rename(columns=cols)
 
-#         for i in range(1, 9):
-#             tt = S_df.loc[:, "Time " + str(i) + " (seconds)"]
-#             vv = S_df.loc[:, "Probe " + str(i) + " (m/s)"]
-#         aax[0].plot((tt - tt[0]) * 1e6, vv.values / 1e3, ".", markersize=2)
+    #     for i in range(1, 9):
+    #         tt = S_df.loc[:, "Time " + str(i) + " (seconds)"]
+    #         vv = S_df.loc[:, "Probe " + str(i) + " (m/s)"]
+    #     aax[0].plot((tt - tt[0]) * 1e6, vv.values / 1e3, ".", markersize=2)
 
-#     ax.set_xlabel(r"$t$ [$\mu$s]")
-#     ax.set_ylabel(r"$v$ [km/s]")
-#     ax.set_xlim([0, 6])
-#     ax = aax[1]
-#     ax.set_xticks(range(0, len(jwlstr)))
-#     ax.set_xticklabels(jwlstr)
-#     f.tight_layout()
-#     plt.show()
+    ax.set_xlabel(r"$t$ [$\mu$s]")
+    ax.set_ylabel(r"$v$ [km/s]")
+    ax.set_xlim([0, 6])
+    ax = aax[1]
+    ax.set_xticks(range(0, len(jwlstr)))
+    ax.set_xticklabels(jwlstr)
+    f.tight_layout()
+    plt.show()
