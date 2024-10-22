e761fa435ed49e8ecb463b412ac5790f7ff51ffd
Kyle Scott Hickmann <hickmank@lanl.gov>
2024-10-22 11:45:03 -0600

    Updated image_prediction_compare to fix some typos and added an input file
    Former-commit-id: 92a5f88338fc7d3bb6c218b4266566c475b044f1

--- applications/evaluation/image_prediction_comparison.py
+++ applications/evaluation/image_prediction_comparison.py
@@ -133,10 +133,9 @@ SAVEFIG = args.savefig
 # Hardcode model hyperparameters for now.
 #
 # NOTE: This should be handled in a MUCH more elegant manner!!!
-kernel = [3, 3]
-featureList = [512, 512, 512, 512, 256, 128, 64, 32]
-linearFeatures = [4, 4]
-initial_learningrate = 0.0007
+#kernel = [3, 3]
+#featureList = [512, 512, 512, 512, 256, 128, 64, 32]
+#linearFeatures = [4, 4]
 
 # model = jekelCNNsurrogate(input_size=29,
 #                           linear_features=linearFeatures,
@@ -145,11 +144,13 @@ initial_learningrate = 0.0007
 #                           output_image_size=(1120, 800),
 #                           act_layer=nn.GELU)
 
-# Set up for study001 and study010 respectively...
+# Set up for study001, study010, study11 respectively...
+# For LSC usually have been using test IDX 689 or 3914
 model = tCNNsurrogate(
     input_size=29,
     #linear_features=(7, 5, 256),
-    linear_features=(7, 5, 512),
+    #linear_features=(7, 5, 512),
+    linear_features=(7, 5, 768),
     initial_tconv_kernel=(5, 5),
     initial_tconv_stride=(5, 5),
     initial_tconv_padding=(0, 0),
@@ -157,7 +158,8 @@ model = tCNNsurrogate(
     initial_tconv_dilation=(1, 1),
     kernel=(3, 3),
     #nfeature_list=[256, 128, 64, 32, 16],
-    nfeature_list=[512, 512, 256, 128, 64],
+    #nfeature_list=[512, 512, 256, 128, 64],
+    nfeature_list=[768, 512, 256, 128, 64],
     output_image_size=(1120, 800),
     act_layer=nn.GELU,
 )
@@ -168,7 +170,7 @@ model = tCNNsurrogate(
 #############################################
 optimizer = torch.optim.AdamW(
     model.parameters(),
-    lr=initial_learningrate,
+    lr=5.0e-03,  # Must be set to load checkpoint but irrelevant for this script.
     betas=(0.9, 0.999),
     eps=1e-08,
     weight_decay=0.01,
