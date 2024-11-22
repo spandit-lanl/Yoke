380c05ba9a41e27d99d8feb79a10a3fb3a3062f0
hickmank <hickmank@lanl.gov>
2024-11-22 11:14:10 -0700

    Ruff linting for new module
    Former-commit-id: db985e13cafda8474e09ff3f25e5c08a6fe37679

--- src/yoke/lr_schedulers.py
+++ src/yoke/lr_schedulers.py
@@ -8,7 +8,6 @@ import math
 import numpy as np
 
 from torch.optim import Optimizer
-from torch.optim.lr_scheduler import LambdaLR
 from torch.optim.lr_scheduler import _LRScheduler
 
 # Imports for plotting
@@ -24,7 +23,6 @@ import matplotlib
 # Get rid of type 3 fonts in figures
 import matplotlib.pyplot as plt
 
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 matplotlib.rcParams["pdf.fonttype"] = 42
 matplotlib.rcParams["ps.fonttype"] = 42
@@ -36,38 +34,37 @@ plt.rcParams["figure.figsize"] = (6, 6)
 
 
 def power_decay_lr_calc(
-        step: int=0,
-        anchor_lr: float=1.0e-3,
-        decay_power: float=0.5,
-        warmup_steps: int=1000,
-        dim_embed: int=1,
+    step: int = 0,
+    anchor_lr: float = 1.0e-3,
+    decay_power: float = 0.5,
+    warmup_steps: int = 1000,
+    dim_embed: int = 1,
 ) -> float:
     """Warm-up with power decay.
 
     Used in `WarmUpPowerDecayScheduler`
-    
-    """
 
+    """
     embed_factor = 1.0 / np.power(float(dim_embed), decay_power)
     anchor = np.power(float(warmup_steps), decay_power)
-    
+
     if step < warmup_steps:
         lr = float(step) / np.power(float(warmup_steps), 1.0 + decay_power)
         lr = anchor * embed_factor * lr
     else:
         lr = 1.0 / np.power(float(step), decay_power)
         lr = anchor * embed_factor * lr
-        
+
     return anchor_lr * lr
 
 
 def cos_decay_lr_calc(
-        step: int=0,
-        anchor_lr: float=1.0e-3,
-        warmup_steps: int=100,
-        terminal_steps: int=1000,
-        min_fraction: float=0.5,
-        num_cycles: float=0.5
+    step: int = 0,
+    anchor_lr: float = 1.0e-3,
+    warmup_steps: int = 100,
+    terminal_steps: int = 1000,
+    min_fraction: float = 0.5,
+    num_cycles: float = 0.5,
 ) -> float:
     """Cosine decay, linear warmup.
 
@@ -75,12 +72,12 @@ def cos_decay_lr_calc(
 
     """
     if step < warmup_steps:
-        lr  = float(step) / float(max(1, warmup_steps))
+        lr = float(step) / float(max(1, warmup_steps))
     else:
         progress = float(step - warmup_steps)
         progress = progress / float(max(1, terminal_steps - warmup_steps))
 
-        tmp_lr = (1.0 + math.cos(math.pi * num_cycles * 2.0 * progress))
+        tmp_lr = 1.0 + math.cos(math.pi * num_cycles * 2.0 * progress)
         tmp_lr = 0.5 * (1.0 - min_fraction) * tmp_lr
         lr = max(0.0, tmp_lr)
         lr = min_fraction + lr
@@ -89,9 +86,7 @@ def cos_decay_lr_calc(
 
 
 def warmup_to_constant_lr(
-        step: int=0,
-        warmup_steps: int=100,
-        lr_constant: float=1e-3
+    step: int = 0, warmup_steps: int = 100, lr_constant: float = 1e-3
 ) -> float:
     """Linear warm up to constant LR.
 
@@ -102,13 +97,13 @@ def warmup_to_constant_lr(
         lr = lr_constant * float(step) / float(max(1.0, warmup_steps))
     else:
         lr = lr_constant
-        
+
     return lr
 
 
 class WarmUpPowerDecayScheduler(_LRScheduler):
     """Scheduler with warm-up and power decay.
-    
+
     Scheduler mentioned in *https://kikaben.com/transformers-training-details*
 
     This goes through a linear warm-up phase for the first `warmup_steps` in
@@ -126,15 +121,16 @@ class WarmUpPowerDecayScheduler(_LRScheduler):
         verbose (bool): Verbosity of scheduler
 
     """
+
     def __init__(
-            self, 
-            optimizer: Optimizer,
-            anchor_lr: float=1.0e-3,
-            decay_power: float=0.5,
-            warmup_steps: int=1000,
-            dim_embed: int=1,
-            last_epoch: int=-1,
-            verbose: bool=False
+        self,
+        optimizer: Optimizer,
+        anchor_lr: float = 1.0e-3,
+        decay_power: float = 0.5,
+        warmup_steps: int = 1000,
+        dim_embed: int = 1,
+        last_epoch: int = -1,
+        verbose: bool = False,
     ) -> None:
         """Initialize scheduler."""
         self.anchor_lr = anchor_lr
@@ -144,7 +140,7 @@ class WarmUpPowerDecayScheduler(_LRScheduler):
         self.num_param_groups = len(optimizer.param_groups)
 
         super().__init__(optimizer, last_epoch, verbose)
-        
+
     def get_lr(self) -> float:
         """Return learning rate."""
         lr = power_decay_lr_calc(
@@ -152,7 +148,7 @@ class WarmUpPowerDecayScheduler(_LRScheduler):
             anchor_lr=self.anchor_lr,
             decay_power=self.decay_power,
             dim_embed=self.dim_embed,
-            warmup_steps=self.warmup_steps
+            warmup_steps=self.warmup_steps,
         )
         return [lr] * self.num_param_groups
 
@@ -179,16 +175,17 @@ class CosineWithWarmupScheduler(_LRScheduler):
         verbose (bool): Verbosity of scheduler
 
     """
+
     def __init__(
         self,
         optimizer: Optimizer,
-        anchor_lr: float=1.0e-3,
-        terminal_steps: int=1000,
-        warmup_steps: int=100,
-        num_cycles: float=0.5,
-        min_fraction: float=0.5,
-        last_epoch: int=-1,
-        verbose: bool=False
+        anchor_lr: float = 1.0e-3,
+        terminal_steps: int = 1000,
+        warmup_steps: int = 100,
+        num_cycles: float = 0.5,
+        min_fraction: float = 0.5,
+        last_epoch: int = -1,
+        verbose: bool = False,
     ) -> None:
         """Initialize scheduler."""
         self.anchor_lr = anchor_lr
@@ -196,9 +193,9 @@ class CosineWithWarmupScheduler(_LRScheduler):
         self.num_cycles = num_cycles
         self.min_fraction = min_fraction
         self.terminal_steps = terminal_steps
-        
+
         self.num_param_groups = len(optimizer.param_groups)
-        
+
         super().__init__(optimizer, last_epoch, verbose)
 
     def get_lr(self) -> float:
@@ -209,13 +206,13 @@ class CosineWithWarmupScheduler(_LRScheduler):
             warmup_steps=self.warmup_steps,
             terminal_steps=self.terminal_steps,
             min_fraction=self.min_fraction,
-            num_cycles=self.num_cycles
-            )
+            num_cycles=self.num_cycles,
+        )
         return [lr] * self.num_param_groups
 
 
 class ConstantWithWarmupScheduler(_LRScheduler):
-    """Constant LR after warmup
+    """Constant LR after warmup.
 
     From:
     https://github.com/krasserm/perceiver-io/blob/main/perceiver/scripts/lrs.py
@@ -228,28 +225,29 @@ class ConstantWithWarmupScheduler(_LRScheduler):
         verbose (bool): Verbosity of scheduler
 
     """
+
     def __init__(
-            self,
-            optimizer: Optimizer,
-            warmup_steps: int=100,
-            lr_constant: float=1e-3,
-            last_epoch: int=-1,
-            verbose: bool=False
+        self,
+        optimizer: Optimizer,
+        warmup_steps: int = 100,
+        lr_constant: float = 1e-3,
+        last_epoch: int = -1,
+        verbose: bool = False,
     ) -> None:
-        """Initialize scheduler"""
+        """Initialize scheduler."""
         self.warmup_steps = warmup_steps
         self.lr_constant = lr_constant
         self.num_param_groups = len(optimizer.param_groups)
-        
+
         super().__init__(optimizer, last_epoch, verbose)
-        
+
     def get_lr(self) -> float:
         """Return learning rate."""
         lr = warmup_to_constant_lr(
             step=self._step_count,
             warmup_steps=self.warmup_steps,
-            lr_constant=self.lr_constant
-            )
+            lr_constant=self.lr_constant,
+        )
 
         return [lr] * self.num_param_groups
 
@@ -258,20 +256,23 @@ if __name__ == "__main__":
     """Plotting for schedulers."""
 
     # Total steps
-    steps = np.arange(0, 2000+1)
+    steps = np.arange(0, 2000 + 1)
     warmup_steps = 500
     anchor_lr = 1.0e-3
 
     # Power decay
     decay_power = 0.6
     dim_embed = 1
-    power_decay = [power_decay_lr_calc(
-        step=step,
-        anchor_lr=anchor_lr,
-        decay_power=decay_power,
-        dim_embed=dim_embed,
-        warmup_steps=warmup_steps
-    ) for step in steps]
+    power_decay = [
+        power_decay_lr_calc(
+            step=step,
+            anchor_lr=anchor_lr,
+            decay_power=decay_power,
+            dim_embed=dim_embed,
+            warmup_steps=warmup_steps,
+        )
+        for step in steps
+    ]
 
     fig1 = plt.figure(figsize=(10, 6))
     plt.plot(steps, power_decay, label="Power-decay LR")
@@ -285,14 +286,17 @@ if __name__ == "__main__":
     min_fraction = 0.7
     num_cycles = 0.5
 
-    cos_decay = [cos_decay_lr_calc(
-        step=step,
-        anchor_lr=anchor_lr,
-        warmup_steps=warmup_steps,
-        terminal_steps=terminal_steps,
-        min_fraction=min_fraction,
-        num_cycles=num_cycles
-    ) for step in steps]
+    cos_decay = [
+        cos_decay_lr_calc(
+            step=step,
+            anchor_lr=anchor_lr,
+            warmup_steps=warmup_steps,
+            terminal_steps=terminal_steps,
+            min_fraction=min_fraction,
+            num_cycles=num_cycles,
+        )
+        for step in steps
+    ]
 
     fig2 = plt.figure(figsize=(10, 6))
     plt.plot(steps, cos_decay, label="Cosine-decay LR")
@@ -302,11 +306,12 @@ if __name__ == "__main__":
     plt.grid()
 
     # Constant with warm-up
-    warmup_constant = [warmup_to_constant_lr(
-        step=step,
-        warmup_steps=warmup_steps,
-        lr_constant=anchor_lr
-    ) for step in steps]
+    warmup_constant = [
+        warmup_to_constant_lr(
+            step=step, warmup_steps=warmup_steps, lr_constant=anchor_lr
+        )
+        for step in steps
+    ]
 
     fig3 = plt.figure(figsize=(10, 6))
     plt.plot(steps, warmup_constant, label="Warm-up to Constant LR")
