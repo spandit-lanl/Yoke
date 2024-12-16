25b2e8fdc13e08f3e274217c686e33a395f20820
Kyle Hickmann <hickmank@lanl.gov>
2024-12-15 22:19:14 -0700

    Began prototyping LodeRunner prediction animation script
    Former-commit-id: d36a3136ffb6e193ac2f1ff958d057fcbffa482b

---
+++ applications/evaluation/lsc_loderunner_anime.py
@@ -0,0 +1,262 @@
+"""Script to produce animation of LodeRunner prediction.
+
+This script allows production of an animation of a single hydrodynamic field
+within one lsc240420 simulation set of NPZ files. 
+
+Three types of images are produced:
+
+    - Ground truth
+    - LodeRunner Checkpoint prediction
+    - Discrepancy
+
+"""
+
+import os
+import glob
+import argparse
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from yoke.models.vit.swin.bomberman import LodeRunner
+from yoke.datasets.lsc_dataset import LSC_halfimage_DataSet
+from yoke.datasets.lsc_dataset import LSCnpz2key
+import yoke.torch_training_utils as tr
+
+# Imports for plotting
+# To view possible matplotlib backends use
+# >>> import matplotlib
+# >>> bklist = matplotlib.rcsetup.interactive_bk
+# >>> print(bklist)
+import matplotlib
+
+# matplotlib.use('MacOSX')
+# matplotlib.use('pdf')
+# matplotlib.use('QtAgg')
+# Get rid of type 3 fonts in figures
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+matplotlib.rcParams["pdf.fonttype"] = 42
+matplotlib.rcParams["ps.fonttype"] = 42
+
+# Ensure LaTeX font
+font = {"family": "serif"}
+plt.rc("font", **font)
+plt.rcParams["figure.figsize"] = (6, 6)
+
+
+###################################################################
+# Define command line argument parser
+descr_str = "Create animation of single hydro-field for LodeRunner on lsc240420 simulation IDX."
+parser = argparse.ArgumentParser(prog="Animation of LodeRunner", description=descr_str)
+
+parser.add_argument(
+    "--checkpoint",
+    action="store",
+    type=str,
+    default="./study018_modelState_epoch0100.hdf5",
+    help="Name of HDF5 model checkpoint to evaluate output for.",
+)
+
+# indir
+parser.add_argument(
+    "--indir",
+    "-D",
+    action="store",
+    type=str,
+    default="/lustre/vescratch1/exempt/artimis/mpmm/lsc240420/",
+    help="Directory to find NPZ files.",
+)
+
+# outdir
+parser.add_argument(
+    "--outdir",
+    "-O",
+    action="store",
+    type=str,
+    default="./",
+    help="Directory to output images to.",
+)
+
+# run index
+# Example: lsc240420_id00201_pvi_idx00100.npz
+parser.add_argument(
+    "--runID",
+    "-R",
+    action="store",
+    type=int,
+    default=201,
+    help="Run identifier index.",
+)
+
+parser.add_argument(
+    "--verbose",
+    "-V",
+    action="store_true",
+    help="Flag to turn on debugging output."
+)
+
+
+def print_NPZ_keys(npzfile: str="./lsc240420_id00201_pvi_idx00100.npz") -> None:
+    """Print keys of NPZ file."""
+    NPZ = np.load(npzfile)
+    print("NPZ file keys:")
+    for key in NPZ.keys():
+        print(key)
+
+    NPZ.close()
+
+    return
+
+
+def singlePVIarray(
+    npzfile: str="./lsc240420_id00201_pvi_idx00100.npz", FIELD: str="av_density"
+) -> np.array:
+    """Function to grab single array from NPZ.
+
+    Args:
+       npzfile (str): File name for NPZ.
+       FIELD (str): Field to return array for.
+
+    Returns:
+       field (np.array): Array of hydro-dynamic field for plotting
+
+    """
+    NPZ = np.load(npzfile)
+    arrays_dict = dict()
+    for key in NPZ.keys():
+        arrays_dict[key] = NPZ[key]
+
+    NPZ.close()
+
+    return arrays_dict[FIELD]
+
+
+if __name__ == "__main__":
+    # Parse commandline arguments
+    args_ns = parser.parse_args()
+
+    # Assign command-line arguments
+    checkpoint = args_ns.checkpoint
+    indir = args_ns.indir
+    outdir = args_ns.outdir
+    runID = args_ns.runID
+    VERBOSE = args_ns.verbose
+
+    # Assemble filenames
+    # Example: lsc240420_id00201_pvi_idx00100.npz
+    npz_glob = os.path.join(indir, f"lsc240420_id{runID:05d}_pvi_idx?????.npz")
+    npz_list = sorted(glob.glob(npz_glob))
+
+    if VERBOSE:
+        print("NPZ files:", npz_list)
+
+    # Instantiate model from checkpoint
+    default_vars = ['density_case',
+                    'density_cushion',
+                    'density_maincharge',
+                    'density_outside_air',
+                    'density_striker',
+                    'density_throw',
+                    'Uvelocity',
+                    'Wvelocity']
+    embed_dim = 512
+    block_structure = (1, 1, 11, 2,)
+    model = LodeRunner(
+        default_vars=default_vars,
+        image_size=(1120, 400),
+        patch_size=(10, 5),  # Since using half-image, halve patch size.
+        embed_dim=embed_dim,
+        emb_factor=2,
+        num_heads=8,
+        block_structure=block_structure,
+        window_sizes=[
+            (8, 8),
+            (8, 8),
+            (4, 4),
+            (2, 2),
+        ],
+        patch_merge_scales=[
+            (2, 2),
+            (2, 2),
+            (2, 2),
+        ],
+    )
+
+    # Load weights from checkpoint
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=1e-6,
+        betas=(0.9, 0.999),
+        eps=1e-08,
+        weight_decay=0.01,
+    )
+
+    checkpoint_epoch = tr.load_model_and_optimizer_hdf5(model, optimizer, checkpoint)
+    model.eval()
+
+    # Build input data
+    in_vars = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7])
+    out_vars = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7])
+
+    input_img_list = []
+    for hfield in default_vars:
+        tmp_img = singlePVIarray(npzfile=npz_list[0], FIELD=hfield)
+
+        # Remember to replace all NaNs with 0.0
+        tmp_img = np.nan_to_num(tmp_img, nan=0.0)
+        input_img_list.append(tmp_img)
+
+    # Concatenate images channel first.
+    input_img = torch.tensor(np.stack(input_img_list, axis=0)).to(torch.float32)
+
+    # Time offset
+    Dt = torch.tensor([0.25])
+
+    # Make a prediction
+    pred_img = model(torch.unsqueeze(input_img, 0), in_vars, out_vars, Dt)
+    pred_rho = np.squeeze(pred_img.detach().numpy()).sum(0)
+    print('Shape LodeRunner out:', pred_rho.shape)
+
+    # for k, npzfile in enumerate(npz_list):
+    #     if k == 0:
+    #         # Do evaluation of LodeRunner from ICs
+    #     else:
+    #         # Evaluate LodeRunner from last prediction
+    #
+    #     # Get index
+    #     pviIDX = npzfile.split('idx')[1]
+    #     pviIDX = int(pviIDX.split('.')[0])
+
+    #     # Get the fields
+    #     Hfield = singlePVIarray(npzfile=npzfile, FIELD=FIELD)
+    #     simtime = singlePVIarray(npzfile=npzfile, FIELD="sim_time")
+    #     Rcoord = singlePVIarray(npzfile=npzfile, FIELD="Rcoord")
+    #     Zcoord = singlePVIarray(npzfile=npzfile, FIELD="Zcoord")
+
+    #     # Plot normalized radiograph and density field for diagnostics.
+    #     fig1, ax1 = plt.subplots(1, 1, figsize=(12, 12))
+    #     img1 = ax1.imshow(
+    #         Hfield,
+    #         aspect="equal",
+    #         extent=[0.0, Rcoord.max(), Zcoord.min(), Zcoord.max()],
+    #         origin="lower",
+    #         cmap="jet",
+    #     )
+    #     ax1.set_ylabel("Z-axis (cm)", fontsize=16)
+    #     ax1.set_xlabel("R-axis (cm)", fontsize=16)
+    #     ax1.set_title(f"T={float(simtime):.2f}us", fontsize=18)
+
+    #     divider1 = make_axes_locatable(ax1)
+    #     cax1 = divider1.append_axes("right", size="10%", pad=0.1)
+    #     fig1.colorbar(img1, cax=cax1).set_label(f"{FIELD}", fontsize=14)
+
+    #     fig1.savefig(
+    #         os.path.join(outdir,
+    #                      f"lsc240420_id{runID:05d}_{FIELD}_idx{pviIDX:05d}.png"),
+    #         bbox_inches="tight",
+    #     )
+    #     plt.close()
