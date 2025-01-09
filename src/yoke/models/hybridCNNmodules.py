c5945613d3a35f3a1dd22fd572f4a400cb0597b1
hickmank <hickmank@lanl.gov>
2025-01-09 15:43:03 -0700

    Finished prototype CNN to be used as Value network.
    Former-commit-id: 726a9069d66e3f2ac4314de2caec0408972a4235

--- src/yoke/models/hybridCNNmodules.py
+++ src/yoke/models/hybridCNNmodules.py
@@ -15,6 +15,8 @@ from yoke.torch_training_utils import count_torch_params
 from yoke.models.CNNmodules import CNN_Interpretability_Module
 from yoke.models.CNNmodules import CNN_Reduction_Module
 
+from yoke.torch_training_utils import count_torch_params
+    
 
 class generalMLP(nn.Module):
     """A general multi-layer perceptron structure.
@@ -67,13 +69,23 @@ class generalMLP(nn.Module):
 
             # Make list of small sequential modules. Then we'll use enumerate
             # in forward method.
-            cmpd_dict = OrderedDict(
-                [
-                    (f"linear{i:02d}", linear),
-                    (f"norm{i:02d}", normalize),
-                    (f"act{i}", activation),
-                ]
-            )
+            # 
+            # Don't attach an activation to the final layer
+            if i == len(self.feature_list) - 2:
+                cmpd_dict = OrderedDict(
+                    [
+                        (f"linear{i:02d}", linear),
+                    ]
+                )
+            else:
+                cmpd_dict = OrderedDict(
+                    [
+                        (f"linear{i:02d}", linear),
+                        (f"norm{i:02d}", normalize),
+                        (f"act{i}", activation),
+                    ]
+                )
+                
             self.LayerList.append(nn.Sequential(cmpd_dict))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -95,13 +107,21 @@ class hybrid2vectorCNN(nn.Module):
     before concatenation to a dense network.
     
     Args:
-        input_size (int): Size of input
-        kernel (tuple[int, int]): Size of convolutional kernels
-        nfeature_list (list[int]): List of number of features in each
-                                   convolutional layer
+        img_size (tuple[int, int, int]): (C, H, W) dimensions of H1 and H2.
+        input_vector_size (int): Size of input vector
         output_dim (int): Dimension of vector output.
-        act_layer(nn.modules.activation): torch neural network layer class
-                                          to use as activation
+        features (int): Number of output channels/features for each convolutional layer.
+        depth (int): Number of convolutional layers in each image processing branch.
+        kernel (int): Size of symmetric convolutional kernels
+        img_embed_dim (int): Number of features in MLP output from image embeddings.
+        vector_embed_dim (int): Number of features in MLP output from image embeddings.
+        vector_feature_list (list[int]): Number of features in each hidden layer of
+                                         vector-MLP.
+        output_feature_list (list[int]): Number of features in each hidden layer of
+                                         final/output-MLP.
+        act_layer(nn.Module): torch neural network layer class to use as activation
+        norm_layer(nn.Module): torch neural network layer class to use as normalization
+                               between MLP layers.
 
     """
 
@@ -111,14 +131,15 @@ class hybrid2vectorCNN(nn.Module):
         input_vector_size: int = 28,
         output_dim: int = 1,
         features: int = 12,
-        lin_embed_features: int = 32,
-        vector_embed_dim: int = 32,
-        vector_feature_list: list[int] = [32, 32, 64, 64],
-        output_feature_list: list[int] = [64, 128, 128, 64],
         depth: int = 12,
         kernel: int = 3,
+        img_embed_dim: int = 32,
+        vector_embed_dim: int = 32,
         size_reduce_threshold: tuple[int, int] = (8, 8),            
+        vector_feature_list: list[int] = [32, 32, 64, 64],
+        output_feature_list: list[int] = [64, 128, 128, 64],
         act_layer: nn.Module = nn.GELU,
+        norm_layer: nn.Module = nn.LayerNorm,
     ) -> None:
         """Initialization for hybrid CNN."""
         super().__init__()
@@ -127,7 +148,7 @@ class hybrid2vectorCNN(nn.Module):
         _, H, W = self.img_size
         self.kernel = kernel
         self.features = features
-        self.lin_embed_features = lin_embed_features
+        self.img_embed_dim = img_embed_dim
         self.vector_embed_dim = vector_embed_dim
         self.vector_feature_list = vector_feature_list
         self.output_feature_list = output_feature_list
@@ -136,6 +157,7 @@ class hybrid2vectorCNN(nn.Module):
         self.input_vector_size = input_vector_size
         self.output_dim = output_dim
         self.act_layer = act_layer
+        self.norm_layer = norm_layer
         
         # CNN processing branch for H1
         self.interpH1 = CNN_Interpretability_Module(
@@ -165,11 +187,14 @@ class hybrid2vectorCNN(nn.Module):
         # Linear embedding H1
         self.lin_embed_h1 = generalMLP(
             input_dim=self.finalH_h1 * self.finalW_h1 * self.features,
-            output_dim=self.lin_embed_features,
-            hidden_feature_list=[2 * self.lin_embed_features],
-            act_layer=nn.GELU,
-            norm_layer=nn.Identity,
+            output_dim=self.img_embed_dim,
+            hidden_feature_list=[2 * self.img_embed_dim],
+            act_layer=self.act_layer,
+            norm_layer=self.norm_layer,
         )
+
+        # Image embed will end with a GELU activation
+        self.h1_embed_act = self.act_layer()
 
         # CNN processing branch for H2
         self.interpH2 = CNN_Interpretability_Module(
@@ -199,31 +224,36 @@ class hybrid2vectorCNN(nn.Module):
         # Linear embedding H2
         self.lin_embed_h2 = generalMLP(
             input_dim=self.finalH_h2 * self.finalW_h2 * self.features,
-            output_dim=self.lin_embed_features,
-            hidden_feature_list=[2 * self.lin_embed_features],
-            act_layer=nn.GELU,
-            norm_layer=nn.Identity,
+            output_dim=self.img_embed_dim,
+            hidden_feature_list=[2 * self.img_embed_dim],
+            act_layer=self.act_layer,
+            norm_layer=self.norm_layer,
         )
 
+        # Image embed will end with a GELU activation
+        self.h2_embed_act = self.act_layer()
+        
         # MLP for processing vector input
         self.vector_mlp = generalMLP(
             input_dim=self.input_vector_size,
             output_dim=self.vector_embed_dim,
             hidden_feature_list=self.vector_feature_list,
-            act_layer=nn.GELU,
-            norm_layer=nn.LayerNorm,
+            act_layer=self.act_layer,
+            norm_layer=self.norm_layer,
         )
+
+        self.vector_embed_act = self.act_layer()
 
         # Final MLP
         #
-        # NOTE: Final activation should somehow be changed to identity.
-        cat_size = self.vector_embed_dim + 2 * self.lin_embed_features
+        # NOTE: Final activation is just identity.
+        cat_size = self.vector_embed_dim + 2 * self.img_embed_dim
         self.final_mlp = generalMLP(
             input_dim=cat_size,
             output_dim=self.output_dim,
             hidden_feature_list=self.output_feature_list,
-            act_layer=nn.GELU,
-            norm_layer=nn.LayerNorm,
+            act_layer=self.act_layer,
+            norm_layer=nn.Identity,
         )
 
     def forward(
@@ -239,16 +269,19 @@ class hybrid2vectorCNN(nn.Module):
         h1_out = self.reduceH1(h1_out)
         h1_out = torch.flatten(h1_out, start_dim=1)
         h1_out = self.lin_embed_h1(h1_out)
-
+        h1_out = self.h2_embed_act(h1_out)
+        
         # Process second image
         h2_out = self.interpH2(h2)
         h2_out = self.reduceH2(h2_out)
         h2_out = torch.flatten(h2_out, start_dim=1)
         h2_out = self.lin_embed_h2(h2_out)
-
+        h2_out = self.h2_embed_act(h2_out)
+        
         # Process vector
         y_out = self.vector_mlp(y)
-
+        y_out = self.vector_embed_act(y_out)
+        
         # Concatenate outputs and send through final MLP layer.
         cat = torch.cat((y_out, h1_out, h2_out), dim=1)
         out = self.final_mlp(cat)
@@ -276,7 +309,7 @@ if __name__ == "__main__":
         input_vector_size=input_vector_size,
         output_dim=output_dim,
         features=12,
-        lin_embed_features=32,
+        img_embed_dim=32,
         vector_embed_dim=32,
         vector_feature_list=[32, 32, 64, 64],
         depth=12,
@@ -288,10 +321,5 @@ if __name__ == "__main__":
     value_model.eval()
     value_pred = value_model(y, H1, H2)
     print("value_pred shape:", value_pred.shape)
-    
-    # pred_y, pred_H1, pred_H2 = value_pred
-
-    # print("pred_y shape:", pred_y.shape)
-    # print("pred_H1 shape:", pred_H1.shape)
-    # print("pred_H2 shape:", pred_H2.shape)
-
+    print("Number of trainable parameters in value network:",
+          count_torch_params(value_model, trainable=True))
