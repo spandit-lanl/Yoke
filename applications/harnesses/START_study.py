2de2481cb4660a3e9e10fe9e167a0a64078533fa
David J. Schodt <dschodt@lanl.gov>
2025-01-29 12:55:06 -0700

    Move a string helper to helpers module
    Former-commit-id: 734fb27c6503ea9908247cc42f4b0448306d2c48

--- applications/harnesses/START_study.py
+++ applications/harnesses/START_study.py
@@ -6,40 +6,9 @@
 import os
 import shutil
 import argparse
-import numpy as np
 import pandas as pd
 
-
-####################################
-# Helper Function
-####################################
-def replace_keys(study_dict: dict, data: str) -> str:
-    """Function to replace "key" values in a string with dictionary values.
-
-    Args:
-        study_dict (dict): dictonary of keys and values to replace
-        data (str): data to replace keys in
-
-    Returns:
-        data (str): data with keys replaced
-
-    """
-    for key, value in study_dict.items():
-        if key == "studyIDX":
-            data = data.replace(f"<{key}>", f"{value:03d}")
-        elif isinstance(value, np.float64) or isinstance(value, float):
-            data = data.replace(f"<{key}>", f"{value:5.4f}")
-        elif isinstance(value, np.int64) or isinstance(value, int):
-            data = data.replace(f"<{key}>", f"{value:d}")
-        elif isinstance(value, str):
-            data = data.replace(f"<{key}>", f"{value}")
-        elif isinstance(value, np.bool_) or isinstance(value, bool):
-            data = data.replace(f"<{key}>", f"{str(value)}")
-        else:
-            print("Key is", key, "with value of", value, "with type", type(value))
-            raise ValueError("Unrecognized datatype in hyperparameter list.")
-
-    return data
+from src.yoke.helpers import replace_keys
 
 
 ####################################
