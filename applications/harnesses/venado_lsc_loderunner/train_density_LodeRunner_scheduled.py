22112e9b2fbf0800a2e3bf0752e6a54f0e0dbc97
Sharmistha Chakrabarti <schakrabarti@lanl.gov>
2025-02-06 15:22:58 -0700

    Implement scheduled sampling to loderunner.
    Former-commit-id: e7db3928a854c4d6b3d0f48a67e78832caa9b4ce

---
+++ applications/harnesses/venado_lsc_loderunner/train_density_LodeRunner_scheduled.py
@@ -0,0 +1,564 @@
+"""Initial training setup for LodeRunner on LSC material densities.
+
+This version of training uses only the lsc240420 data with only per-material
+density along with the velocity field. A single timestep is input, a single
+timestep is predicted. The number of input variables is fixed throughout
+training.
+
+"""
+
+#############################################
+# Packages
+#############################################
+import os
+import time
+import argparse
+import torch
+import torch.nn as nn
+
+from yoke.models.vit.swin.bomberman import LodeRunner
+from yoke.datasets.lsc_dataset import LSC_rho2rho_sequential_DataSet
+import yoke.torch_training_utils as tr
+from yoke.parallel_utils import LodeRunner_DataParallel
+
+
+#############################################
+# Inputs
+#############################################
+descr_str = (
+    "Trains LodeRunner architecture using scheduled sampling "
+    "with sequential inputs and outputs. "
+    "This training uses lsc240420 data for per-material density fields."
+)
+parser = argparse.ArgumentParser(
+    prog="Initial LodeRunner Training", description=descr_str, fromfile_prefix_chars="@"
+)
+
+#############################################
+# Data Parallelism
+#############################################
+parser.add_argument(
+    '--multigpu',
+    action='store_true',
+    help='Supports multiple GPUs on a single node.'
+)
+
+#############################################
+# Learning Problem
+#############################################
+parser.add_argument(
+    "--studyIDX",
+    action="store",
+    type=int,
+    default=1,
+    help="Study ID number to match hyperparameters",
+)
+
+#############################################
+# File Paths
+#############################################
+parser.add_argument(
+    "--FILELIST_DIR",
+    action="store",
+    type=str,
+    default=os.path.join(os.path.dirname(__file__), "../../filelists/"),
+    help="Directory where filelists are located.",
+)
+
+parser.add_argument(
+    "--LSC_NPZ_DIR",
+    action="store",
+    type=str,
+    default=os.path.join(os.path.dirname(__file__), "../../../data_examples/lsc240420/"),
+    help="Directory in which LSC *.npz files live.",
+)
+
+parser.add_argument(
+    "--max_timeIDX_offset",
+    action="store",
+    type=int,
+    default=1,
+    help="Maximum time index offset to attempt prediction for in the dataset."
+)
+
+parser.add_argument(
+    "--max_file_checks",
+    action="store",
+    type=int,
+    default=5,
+    help="Maximum number of file existence checks in the dataset."
+)
+
+parser.add_argument(
+    "--train_filelist",
+    action="store",
+    type=str,
+    default="lsc240420_prefixes_train_80pct.txt",
+    help="Path to list of files to train on.",
+)
+
+parser.add_argument(
+    "--validation_filelist",
+    action="store",
+    type=str,
+    default="lsc240420_prefixes_validation_10pct.txt",
+    help="Path to list of files to validate on.",
+)
+
+parser.add_argument(
+    "--test_filelist",
+    action="store",
+    type=str,
+    default="lsc240420_prefixes_test_10pct.txt",
+    help="Path to list of files to test on.",
+)
+
+#############################################
+# Model Parameters
+#############################################
+parser.add_argument(
+    "--block_structure",
+    action="store",
+    type=int,
+    nargs="+",
+    default=[1, 1, 3, 1],
+    help="List of number of SW-MSA layers in each SWIN block.",
+)
+
+parser.add_argument(
+    "--embed_dim",
+    action="store",
+    type=int,
+    default=128,
+    help="Initial embedding dimension for SWIN-Unet.",
+)
+
+#############################################
+# Training Parameters
+#############################################
+parser.add_argument(
+    "--init_learnrate",
+    action="store",
+    type=float,
+    default=1e-3,
+    help="Initial learning rate",
+)
+
+parser.add_argument(
+    "--LRepoch_per_step",
+    action="store",
+    type=float,
+    default=10,
+    help="Number of epochs per LR reduction.",
+)
+
+parser.add_argument(
+    "--LRdecay", action="store", type=float, default=0.5, help="LR decay factor."
+)
+
+parser.add_argument(
+    "--batch_size", action="store", type=int, default=64, help="Batch size"
+)
+
+parser.add_argument(
+    "--num_workers",
+    action="store",
+    type=int,
+    default=4,
+    help=("Number of processes simultaneously loading batches of data. "
+          "NOTE: If set too big workers will swamp memory!!")
+)
+
+parser.add_argument(
+    "--prefetch_factor",
+    action="store",
+    type=int,
+    default=2,
+    help=("Number of batches each worker preloads ahead of time. "
+          "NOTE: If set too big preload will swamp memory!!")
+)
+
+#############################################
+# Scheduled Sampling Parameters
+#############################################
+parser.add_argument(
+    "--scheduled_prob",
+    action="store",
+    type=float,
+    default=1.0,  # Initial probability of using ground truth
+    help="Initial probability of using ground truth for scheduled sampling."
+)
+
+parser.add_argument(
+    "--decay_rate",
+    action="store",
+    type=float,
+    default=1.0,  # Decay rate for scheduled_prob
+    help="Schedule probability multiplier after each epoch."
+)
+
+parser.add_argument(
+    "--minimum_schedule_prob",
+    action="store",
+    type=float,
+    default=0.0,  # Initial probability of using ground truth
+    help="Minimum scheduled-sampling probability."
+)
+
+#############################################
+# Epoch Parameters
+#############################################
+parser.add_argument(
+    "--total_epochs", action="store", type=int, default=10, help="Total training epochs"
+)
+
+parser.add_argument(
+    "--cycle_epochs",
+    action="store",
+    type=int,
+    default=5,
+    help=(
+        "Number of epochs between saving the model and re-queueing "
+        "training process; must be able to be completed in the "
+        "set wall time"
+    ),
+)
+
+parser.add_argument(
+    "--train_batches",
+    action="store",
+    type=int,
+    default=250,
+    help="Number of batches to train on in a given epoch",
+)
+
+parser.add_argument(
+    "--val_batches",
+    action="store",
+    type=int,
+    default=25,
+    help="Number of batches to validate on in a given epoch",
+)
+
+parser.add_argument(
+    "--TRAIN_PER_VAL",
+    action="store",
+    type=int,
+    default=10,
+    help="Number of training epochs between each validation epoch",
+)
+
+parser.add_argument(
+    "--trn_rcrd_filename",
+    action="store",
+    type=str,
+    default="./default_training.csv",
+    help="Filename for text file of training loss and metrics on each batch",
+)
+
+parser.add_argument(
+    "--val_rcrd_filename",
+    action="store",
+    type=str,
+    default="./default_validation.csv",
+    help="Filename for text file of validation loss and metrics on each batch",
+)
+
+parser.add_argument(
+    "--continuation",
+    action="store_true",
+    help="Indicates if training is being continued or restarted",
+)
+
+parser.add_argument(
+    "--checkpoint",
+    action="store",
+    type=str,
+    default="None",
+    help="Path to checkpoint to continue training from",
+)
+
+#############################################
+#############################################
+if __name__ == "__main__":
+    #############################################
+    # Process Inputs
+    #############################################
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    args = parser.parse_args()
+
+    #############
+    num_gpus = torch.cuda.device_count()
+    print(f"Number of GPUs available: {num_gpus}")
+    #############
+    # Study ID
+    studyIDX = args.studyIDX
+
+    # Data Paths
+    train_filelist = args.FILELIST_DIR + args.train_filelist
+    validation_filelist = args.FILELIST_DIR + args.validation_filelist
+    test_filelist = args.FILELIST_DIR + args.test_filelist
+
+    # Model Parameters
+    embed_dim = args.embed_dim
+    block_structure = tuple(args.block_structure)
+    
+    # Training Parameters
+    initial_learningrate = args.init_learnrate
+    LRepoch_per_step = args.LRepoch_per_step
+    LRdecay = args.LRdecay
+    batch_size = args.batch_size
+	
+    # Scheduled Sampling Parameters
+    scheduled_prob = args.scheduled_prob
+    decay_rate = args.decay_rate
+    minimum_schedule_prob = args.minimum_schedule_prob
+
+    # Number of workers controls how batches of data are prefetched and,
+    # possibly, pre-loaded onto GPUs. If the number of workers is large they
+    # will swamp memory and jobs will fail.
+    num_workers = args.num_workers
+    prefetch_factor = args.prefetch_factor
+
+    # Epoch Parameters
+    total_epochs = args.total_epochs
+    cycle_epochs = args.cycle_epochs
+    train_batches = args.train_batches
+    val_batches = args.val_batches
+    train_per_val = args.TRAIN_PER_VAL
+    trn_rcrd_filename = args.trn_rcrd_filename
+    val_rcrd_filename = args.val_rcrd_filename
+    CONTINUATION = args.continuation
+    START = not CONTINUATION
+    checkpoint = args.checkpoint
+
+    #############################################
+    # Check Devices
+    #############################################
+    print("\n")
+    print("Slurm & Device Information")
+    print("=========================================")
+    print("Slurm Job ID:", os.environ["SLURM_JOB_ID"])
+    print("Pytorch Cuda Available:", torch.cuda.is_available())
+    print("GPU ID:", os.environ["SLURM_JOB_GPUS"])
+    print("Number of System CPUs:", os.cpu_count())
+    print("Number of CPUs per GPU:", os.environ["SLURM_JOB_CPUS_PER_NODE"])
+
+    print("\n")
+    print("Model Training Information")
+    print("=========================================")
+
+    #############################################
+    # Initialize Model
+    #############################################
+    model = LodeRunner(
+        default_vars=['density_case',
+                      'density_cushion',
+                      'density_maincharge',
+                      'density_outside_air',
+                      'density_striker',
+                      'density_throw',
+                      'Uvelocity',
+                      'Wvelocity'],
+        image_size=(1120, 800),
+        patch_size=(10, 10),
+        embed_dim=embed_dim,
+        emb_factor=2,
+        num_heads=8,
+        block_structure=block_structure,
+        window_sizes=[
+            (8, 8),
+            (8, 8),
+            (4, 4),
+            (2, 2),
+        ],
+        patch_merge_scales=[
+            (2, 2),
+            (2, 2),
+            (2, 2),
+        ],
+    )
+
+    print("Lode Runner parameters:", tr.count_torch_params(model, trainable=True))
+    # Wait to move model to GPU until after the checkpoint load. Then
+    # explicitly move model and optimizer state to GPU.
+
+    #############################################
+    # Initialize Optimizer
+    #############################################
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=initial_learningrate,
+        betas=(0.9, 0.999),
+        eps=1e-08,
+        weight_decay=0.01,
+    )
+
+    #############################################
+    # Initialize Loss
+    #############################################
+    # Use `reduction='none'` so loss on each sample in batch can be recorded.
+    loss_fn = nn.MSELoss(reduction="none")
+
+    print("Model initialized.")
+
+    #############################################
+    # Load Model for Continuation
+    #############################################
+    if CONTINUATION:
+        starting_epoch = tr.load_model_and_optimizer_hdf5(model, optimizer, checkpoint)
+        print("Model state loaded for continuation.")
+    else:
+        starting_epoch = 0
+
+    #############################################
+    # Move model and optimizer state to GPU
+    #############################################
+    if args.multigpu:
+        model = LodeRunner_DataParallel(model)
+
+    model.to(device)
+
+    for state in optimizer.state.values():
+        for k, v in state.items():
+            if isinstance(v, torch.Tensor):
+                state[k] = v.to(device)
+
+    #############################################
+    # Setup LR scheduler
+    #############################################
+    stepLRsched = torch.optim.lr_scheduler.StepLR(
+        optimizer,
+        step_size=LRepoch_per_step,
+        gamma=LRdecay,
+        last_epoch=starting_epoch - 1,
+    )
+    
+    #############################################
+    # Initialize Data
+    #############################################
+    train_dataset = LSC_rho2rho_sequential_DataSet(
+	    LSC_NPZ_DIR=args.LSC_NPZ_DIR,
+    	file_prefix_list=train_filelist,
+    	max_timeIDX_offset=1,  
+    	max_file_checks=10,     
+    	seq_len=3,             # Sequence length
+    	half_image=True
+	)
+
+    val_dataset = LSC_rho2rho_sequential_DataSet(
+        LSC_NPZ_DIR=args.LSC_NPZ_DIR,
+        file_prefix_list=validation_filelist,
+        max_timeIDX_offset=1,  
+        max_file_checks=10,     	
+        seq_len=3,             # Sequence length
+        half_image=True
+	)
+
+    test_dataset = LSC_rho2rho_sequential_DataSet(
+        LSC_NPZ_DIR=args.LSC_NPZ_DIR,
+        file_prefix_list=test_filelist,
+        max_timeIDX_offset=1,  
+        max_file_checks=10,    
+        seq_len=3,             # Sequence length
+        half_image=True
+	)
+
+    print("Datasets initialized...")
+
+    #############################################
+    # Training Loop
+    #############################################
+    # Train Model
+    print("Training Model . . .")
+    starting_epoch += 1
+    ending_epoch = min(starting_epoch + cycle_epochs, total_epochs + 1)
+
+    # Setup Dataloaders
+    train_dataloader = tr.make_dataloader(
+        train_dataset,
+        batch_size,
+        train_batches,
+        num_workers=num_workers,
+        prefetch_factor=prefetch_factor
+    )
+    val_dataloader = tr.make_dataloader(
+        val_dataset,
+        batch_size,
+        val_batches,
+        num_workers=num_workers,
+        prefetch_factor=prefetch_factor
+    )
+    print("DataLoaders initialized...")
+    
+    for epochIDX in range(starting_epoch, ending_epoch):
+        # Time each epoch and print to stdout
+        startTime = time.time()
+
+        torch.cuda.empty_cache()
+        
+        # Train an epoch with scheduled sampling
+        tr.train_scheduled_loderunner_epoch(
+            training_data=train_dataloader,
+            validation_data=val_dataloader,
+            model=model,
+            optimizer=optimizer,
+            loss_fn=loss_fn,
+            epochIDX=epochIDX,
+            train_per_val=train_per_val,
+            train_rcrd_filename=trn_rcrd_filename,
+            val_rcrd_filename=val_rcrd_filename,
+            device=device,
+            scheduled_prob=scheduled_prob,
+        )
+
+        # Increment LR scheduler
+        stepLRsched.step()
+
+        endTime = time.time()
+        epoch_time = (endTime - startTime) / 60
+
+        # Print Summary Results
+        print(f"Completed epoch {epochIDX} with scheduled_prob {scheduled_prob:.4f}...",
+              flush=True)
+        print(f"Epoch time (minutes): {epoch_time:.2f}", flush=True)
+
+        # Decay the scheduled probability
+        scheduled_prob = max(scheduled_prob * decay_rate, minimum_schedule_prob)
+
+        # Clear GPU memory after each epoch
+        torch.cuda.empty_cache()
+
+    # Save Model Checkpoint
+    print("Saving model checkpoint at end of epoch " + str(epochIDX) + ". . .")
+
+    # Move the model back to CPU prior to saving to increase portability
+    model.to("cpu")
+    # Move optimizer state back to CPU
+    for state in optimizer.state.values():
+        for k, v in state.items():
+            if isinstance(v, torch.Tensor):
+                state[k] = v.to("cpu")
+
+    # Save model and optimizer state in hdf5
+    h5_name_str = "study{0:03d}_modelState_epoch{1:04d}.hdf5"
+    new_h5_path = os.path.join("./", h5_name_str.format(studyIDX, epochIDX))
+    tr.save_model_and_optimizer_hdf5(
+        model, optimizer, epochIDX, new_h5_path, compiled=False
+    )
+
+    #############################################
+    # Continue if Necessary
+    #############################################
+    FINISHED_TRAINING = epochIDX + 1 > total_epochs
+    if not FINISHED_TRAINING:
+        new_slurm_file = tr.continuation_setup(
+            new_h5_path, studyIDX, last_epoch=epochIDX
+        )
+        os.system(f"sbatch {new_slurm_file}")
+
+    ###########################################################################
+    # For array prediction, especially large array prediction, the network is
+    # not evaluated on the test set after training. This is performed using
+    # the *evaluation* module as a separate post-analysis step.
+    ###########################################################################
