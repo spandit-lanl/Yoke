5a3e62b1048aa08e65102bdc3d29503ce1236b5f
Kyle Hickmann <hickmank@lanl.gov>
2025-04-18 11:44:27 -0600

    Updated production run parameters after initial timing and batch_size evaluation runs.

--- applications/harnesses/ch_lsc_policy/train_lsc_policy.py
+++ applications/harnesses/ch_lsc_policy/train_lsc_policy.py
@@ -140,7 +140,7 @@ def main(
     #############################################
     optimizer = torch.optim.AdamW(
         model.parameters(),
-        lr=1e-6,
+        lr=1e-3,
         betas=(0.9, 0.999),
         eps=1e-08,
         weight_decay=0.01
